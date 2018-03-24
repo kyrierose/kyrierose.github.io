bf4c34e5ac77f42b7fbf54ae23d342e0ed49d3f8
kyrierose <akshitdhar@live.com>
2018-03-24 22:30:14 +0530

    Update app.js

--- tutorial-extjs/app.js
+++ tutorial-extjs/app.js
@@ -1,3 +1,22 @@
 Ext.onReady(function(){
-        alert("Sample");
+        Ext.create('Ext.data.Store', {
+                    storeId: 'tutorials',
+                    fields:[ 'S.No','Title', 'Link'],
+                    data: [
+                        { no:'1', name: 'What is Ext.JS?', link: 'http://kyrierose.github.io'}
+                    ]
+            });
+
+                Ext.create('Ext.grid.Panel', {
+                    title: 'Extjs Tutorials',
+                    store: Ext.data.StoreManager.lookup('tutorials'),
+                    columns: [
+                        { text: 'S.No', dataIndex: 'no' },
+                        { text: 'Name', dataIndex: 'name' },
+                        { text: 'Link', dataIndex: 'link' },
+                    ],
+                    height: 200,
+                    width: 400,
+                    renderTo: Ext.getBody()
+                });
 });
