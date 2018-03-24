191fec2f2ab490a12952478193a89cb03c7d7e8c
kyrierose <akshitdhar@live.com>
2018-03-24 23:44:52 +0530

    Update app.js

--- tutorial-extjs/app.js
+++ tutorial-extjs/app.js
@@ -3,7 +3,7 @@ Ext.onReady(function(){
                     storeId: 'tutorials',
                     fields:[ 'S.No','Title', 'Link'],
                     data: [
-                        { no:'1', name: 'What is Ext.JS?', link: 'http://kyrierose.github.io/'}
+                        { no:'1', title: 'What is Ext.js?', link: 'http://kyrierose.github.io/updated'}
                     ]
             });
 
@@ -12,7 +12,7 @@ Ext.onReady(function(){
                     store: mainStore,
                     columns: [
                         { text: 'S.No', dataIndex: 'no' },
-                        { text: 'Name', dataIndex: 'name', width:200 },
+                        { text: 'Title', dataIndex: 'title', width:200 },
                         { text: 'Link', dataIndex: 'link' , width: 200},
                     ],
                     style: 'margin:0 auto;',
@@ -21,7 +21,8 @@ Ext.onReady(function(){
                     listeners: {
                         itemclick: function(dv, record, item, index, e) {
                                 var selectedRec = dv.getSelectionModel().getSelected();          
-                                alert(selectedRec.get('name')); //Will display text of name column of selected record
+                                alert(selectedRec.get('link'));
+                                alert(selectedRec.get('Link'));//Will display text of name column of selected record
                         }
                     },
                     width: "50%",
